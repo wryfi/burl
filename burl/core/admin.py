774e8552de6102c0b9345452f7408da8a96650b9
wryfi <1550854+wryfi@users.noreply.github.com>
2022-02-25 12:52:28 -0800

    update admin settings

--- burl/core/admin.py
+++ burl/core/admin.py
@@ -4,3 +4,6 @@ from .models import BurlUser
 
 
 admin.site.register(BurlUser, UserAdmin)
+admin.site.site_header = "burl admin"
+admin.site.site_title = "burl admin"
+admin.site.index_title = "home"
